51e3c8d62eb7b869fe2f03dc99d4de3f85def8f8
TaegyunWoo <dnxprbs@gmail.com>
2021-09-05 17:01:08 +0900

    Change H2 Mode to In-Memory

---
+++ basicweb/src/main/java/web/crud/basic/basicweb/domain/ArticleMapper.java
@@ -0,0 +1,9 @@
+package web.crud.basic.basicweb.domain;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleMapper {
+//    @Insert("INSERT INTO article")
+}
---
+++ basicweb/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user(email, name, password) VALUES ('test@test.com', 'test', '123test!');
+INSERT INTO article(title, content, writer, datetime) VALUES ('TestTitle1', 'TestContent1', 1, '2021-09-05 11:32:22.000003333');
+INSERT INTO article(title, content, writer, datetime) VALUES ('TestTitle2', 'TestContent2', 1, '2021-09-05 12:32:22.000003333');
+INSERT INTO article(title, content, writer, datetime) VALUES ('TestTitle3', 'TestContent3', 1, '2021-09-05 13:32:22.000003333');
